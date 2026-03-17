3e96e0aa06d59ea377b89ac22729eced89850f68
sadproject101 <csitproject101@gmail.com>
2026-03-17 20:06:37 +0545

    signup create faculty

--- backend/login.php
+++ backend/login.php
@@ -1,31 +1,28 @@
 <?php
 include 'dbconnect.php';
-if($_SERVER["REQUEST_METHOD"]=="POST")
-    {
-        $email=$_POST['login-email'];
-        $password=$_POST['password'];
-        $retrieve="Select * from users where email='$email'";
-        $result=mysqli_query($conn,$retrieve);
-        if(!$result)
-            {
-                die("No User Found!");
-            }
-            $user=mysqli_fetch_assoc($result);
-            if(password_verify($password, $user['password']))
-                {
-                    
-                    if($user['user_role'] == 'admin') {
-                        header("Location: ../frontend/admin-dashboard.html");
-                        exit();
-                    } else if($user['user_role'] == 'user') {
-                        header("Location: ../frontend/faculty-dashboard.html");
-                        exit();
-                    }
-                }
-                else
-                    {
-                        die("Invalid Password");
-                        header("Location: ../frontend/login.html");
-                    }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['login-email'];
+    $password = $_POST['password'];
+    $retrieve = "Select * from users where email='$email'";
+    $result = mysqli_query($conn, $retrieve);
+    if (!$result) {
+        die("No User Found!");
     }
+    $user = mysqli_fetch_assoc($result);
+    if (password_verify($password, $user['password'])) {
+
+        if ($user['user_role'] == 'admin') {
+            header("Location: ../frontend/admin-dashboard.html");
+            exit();
+        }
+        else if ($user['user_role'] == 'user') {
+            header("Location: ../frontend/dashboard.html");
+            exit();
+        }
+    }
+    else {
+        die("Invalid Password");
+        header("Location: ../frontend/login.html");
+    }
+}
 ?>
